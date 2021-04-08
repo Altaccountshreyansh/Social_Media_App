43969288a6aebdba2f0339746c31f1ee5b9415cb
Rishabh Raghuwanshi <hasmukchand@gmail.com>
2021-04-08 23:23:46 +0530

    Profile Page File Created

---
+++ frontend/src/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import {Fragment} from 'react';
+
+const Home = () => {
+	return (
+		<Fragment>
+			
+		</Fragment>	
+	)
+}
+
+export default Profile;
